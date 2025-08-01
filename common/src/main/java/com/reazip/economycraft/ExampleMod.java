67452f3a4415e4b6235b4cd851faa72212f3ba36
PhilipB <philip.bartelik@gmail.com>
2025-08-01 18:55:27 +0200

    Initial commit

---
+++ common/src/main/java/com/reazip/economycraft/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.reazip.economycraft;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "economycraft";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/com/reazip/economycraft/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package com.reazip.economycraft.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.reazip.economycraft.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
